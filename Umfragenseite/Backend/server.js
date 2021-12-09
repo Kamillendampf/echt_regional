88fb42f4e3b852a0b37bfe43257fc5b04dde21fc
Kamillendampf <raphael@hhaerle.de>
2021-12-09 23:55:43 +0100

    add Database and made the backend read and write able, all files are importet as JSON

--- Umfragenseite/Backend/server.js
+++ Umfragenseite/Backend/server.js
@@ -1,8 +1,20 @@
-var http = require('http');
-const { compileFunction } = require('vm');
+var http    = require('http');
+const { rejects } = require('assert');
+const { func } = require('assert-plus');
+const { resolve } = require('path/posix');
+const { exit, allowedNodeEnvironmentFlags } = require('process');
+const sqlite3 = require('sqlite3');
+const { clean } = require('nopt');
+service = './database/test.db';
+let db = '';
+var answers = '';
+const sqlINSERT = 'INSERT INTO testt VALUES(?)';
+const sqlSELECTall = 'SELECT * FROM testt';
 
 
+connect();
 var server = http.createServer(function (req, res) {
+
 
  if (req.method === "POST") {
     
@@ -15,15 +27,59 @@ var server = http.createServer(function (req, res) {
         var decode = "";
         req.on("data", function (chunk) {
             body += chunk;
+            insert(body);
             list = JSON.parse(body)
 
-            console.log(list)
+            
+            
         });
 
         req.on("end", function(){
+            
            res.writeHead(200, { "Content-Type": "text/html" });
-            res.end(b);
+            res.end(getAnswers());
         });
     }
 
 }).listen(3000);
+
+
+function connect() {
+        
+   
+    console.log('function connect is called')
+    db = new sqlite3.Database(service, sqlite3.OPEN_READWRITE, (err) =>{
+        if(err){
+            console.error(err);
+            exit(1)
+        }
+        console.log('Connected to Database')
+    });
+}
+
+
+function insert(body) {
+    answers = body;
+    db.run(sqlINSERT, answers, function(err){
+        if (err){
+            console.error(err);
+        }
+        else{
+            console.log("add a new row to your Database")
+        }
+    })
+}
+function getAnswers() {
+    db.all(sqlSELECTall, [], (err, rows) => {
+        if(err){
+            throw err;
+        }else{
+            var clean = [];
+            rows.forEach(row => {
+                clean.push(JSON.parse(row.jsonFile))
+                
+            });
+            return clean;
+        }
+    })
+}
