b9a20ae09ed9170fa239a09e8233e4fa9424c869
Kamillendampf <raphael@hhaerle.de>
2021-12-10 00:10:41 +0100

    add a update, user couldn't click a answer. oherways they get a failure messag

--- Umfragenseite/Frontend/sendToServer.js
+++ Umfragenseite/Frontend/sendToServer.js
@@ -3,13 +3,70 @@ function dataTransfer(){
     let xhr = new XMLHttpRequest();
     const host = "http://localhost:3000";
 
-    const msg0  = document.querySelector('input[name="1"]:checked').value;
-    const msg1 = document.querySelector('input[name="2"]:checked').value;
-    const msg2 = document.querySelector('input[name="3"]:checked').value;
-    const msg3 = document.querySelector('input[name="4"]:checked').value;
-    const msg4 = document.querySelector('input[name="5"]:checked').value;
-    const msg5 = document.querySelector('input[name="6"]:checked').value;
-    const msg6 = document.querySelector('input[name="7"]:checked').value;
+    //------------------------------------------------------------------0
+    if ( document.querySelector('input[name="1"]:checked') != null){
+        msg0 = document.querySelector('input[name="1"]:checked').value;  
+   }
+   else{
+       window.alert("Du hast Frage eins nicht beantwortet");
+       window.stop(0);
+       document.location.reload();
+   }
+  //------------------------------------------------------------------1
+   if ( document.querySelector('input[name="2"]:checked') != null){
+        msg1 = document.querySelector('input[name="2"]:checked').value;  
+  }
+  else{
+      window.alert("Du hast Frage eins nicht beantwortet");
+      window.stop(0);
+      document.location.reload();
+  }
+  //------------------------------------------------------------------2
+  if ( document.querySelector('input[name="3"]:checked') != null){
+        msg2 = document.querySelector('input[name="3"]:checked').value; 
+   }
+   else{
+       window.alert("Du hast Frage eins nicht beantwortet");
+       window.stop(0);
+       document.location.reload();
+   }
+  //------------------------------------------------------------------3
+   if ( document.querySelector('input[name="4"]:checked') != null){
+       msg3 = document.querySelector('input[name="4"]:checked').value; 
+  }
+  else{
+      window.alert("Du hast Frage eins nicht beantwortet");
+      window.stop(0);
+      document.location.reload();
+  }
+  //------------------------------------------------------------------4
+  if ( document.querySelector('input[name="5"]:checked') != null){
+       msg4 = document.querySelector('input[name="5"]:checked').value;
+   }
+   else{
+       window.alert("Du hast Frage eins nicht beantwortet");
+       window.stop(0);
+       document.location.reload();
+   }
+  //------------------------------------------------------------------5
+   if ( document.querySelector('input[name="6"]:checked') != null){
+       msg5 = document.querySelector('input[name="6"]:checked').value;
+   }
+   else{
+      window.alert("Du hast Frage eins nicht beantwortet");
+       window.stop(0);
+      document.location.reload();
+}
+  //------------------------------------------------------------------6
+   if ( document.querySelector('input[name="7"]:checked') != null){
+       msg6 = document.querySelector('input[name="7"]:checked').value;
+   }
+   else{
+       window.alert("Du hast Frage eins nicht beantwortet");
+       window.stop(0);
+       document.location.reload();
+   }
+      //------------------------------------------------------------------6
     const msg7 = document.querySelector("#acht").value;
     const msg8 = document.querySelector('#neun').value;
 
